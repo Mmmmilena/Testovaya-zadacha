7b23d484e953070540a50066ca7c6e37b98d524c
Mmmmilena <148623961+Mmmmilena@users.noreply.github.com>
2023-10-21 19:12:35 +0300

    Delete src directory

--- src/Zadanie10.java
+++
@@ -1,9 +0,0 @@
-public class Zadanie10 {
-    public static void main(String[] args) {
-
-        for (int i = 107; i <= 997; i= i + 10) {
-            System.out.print(i + " ");
-        }
-
-    }
-}
--- src/Zadanie9.java
+++
@@ -1,9 +0,0 @@
-public class Zadanie9 {
-    public static void main(String[] args) {
-        System.out.println("Начало");
-        for (int i = 999; i >= 100; i-- ) {
-            System.out.print(i + " ");
-        }
-        System.out.println("Завершено");
-    }
-}
